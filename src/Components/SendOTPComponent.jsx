29c85efd960f87f392c5a64a17ac043e058f872d
bjoel67 <35962361+bjoel67@users.noreply.github.com>
2022-07-03 01:34:49 +0530

    Add files via upload

--- src/Components/SendOTPComponent.jsx
+++ src/Components/SendOTPComponent.jsx
@@ -2,11 +2,15 @@ import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../Context/AppContext";
 import { listOfSendOTP } from "./Constants";
+import { base, method, body, header } from 'http-client'
+
 
 export const SendOTPComponent = () => {
     let { selectApplication, mobileNumber, setMobileNumber, isRandomSelect, setSelectApplication } = useContext(AppContext);
     // let selctedApp = (listOfSendOTP || []).find(app => app.unique == selectApplication);
     const [isSendinginProcess, setIsSendinginProcess] = useState(false);
+    let createFetch = require('http-client').createFetch
+
     const validateMobileNumber = () => {
         if (!!mobileNumber && mobileNumber.length == 10 && (!isNaN(mobileNumber))) {
             let selctedApp = {};
@@ -40,21 +44,28 @@ export const SendOTPComponent = () => {
     }, [isSendinginProcess])
 
     const sendOtp = (isGet, EndpointUrl, RequestObject) => {
-        var config = {
-            headers:
-            {
-                "Origin": "https://unacademy.com",
-                "Referer": "https://unacademy.com/",
-                "Access-Control-Allow-Origin":"*"
-            }
-        }
+
+        const fetch = createFetch(
+            base(EndpointUrl),
+            method('POST'),
+            header('accept', '*/*'),
+            header('origin', 'https://user.vedantu.com'),
+            header('referer', 'https://user.vedantu.com'),
+            body(JSON.stringify({ "phoneCode": "+91", "phoneNumber": "9550273278" }))
+        )
+
         console.log(isGet, EndpointUrl, RequestObject);
         if (!!isGet) {
             axios.get().then().catch()
         } else {
-            axios.post(EndpointUrl, { ...JSON.parse(RequestObject) }, config)
-                .then((resp) => { console.log(resp) })
-                .catch((err) => { console.log(err) })
+            // axios.post(EndpointUrl, { ...JSON.parse(RequestObject) }, config)
+            //     .then((resp) => { console.log(resp) })
+            //     .catch((err) => { console.log(err) })
+            fetch()
+                .then(response => {
+                    console.log(response)
+                })
+
         }
     }
     return (
